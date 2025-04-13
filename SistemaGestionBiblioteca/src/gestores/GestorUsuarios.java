23c449cca34d0ac40e67aa45ebc7d884733d6893
vicky <vicky@pop-os.localdomain>
2025-04-12 21:19:48 -0300

    Implementacion del gestor de recursos

--- SistemaGestionBiblioteca/src/gestores/GestorUsuarios.java
+++ SistemaGestionBiblioteca/src/gestores/GestorUsuarios.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GestorUsuarios {
-    private List<Usuario> usuarios;
+    private final List<Usuario> usuarios;
 
     public GestorUsuarios() {
         this.usuarios = new ArrayList<>();
