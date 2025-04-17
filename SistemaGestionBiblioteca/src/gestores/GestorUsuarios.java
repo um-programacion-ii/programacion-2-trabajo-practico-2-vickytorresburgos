602d864fe3f8f25960961156a20234d5345698ae
vicky <vicky@pop-os.localdomain>
2025-04-17 19:23:12 -0300

    implementacion de interfaz para notificaciones

--- SistemaGestionBiblioteca/src/gestores/GestorUsuarios.java
+++ SistemaGestionBiblioteca/src/gestores/GestorUsuarios.java
@@ -1,4 +1,4 @@
-package services;
+package gestores;
 
 import models.Usuario;
 
---
+++ SistemaGestionBiblioteca/src/services/NotificacionesService.java
@@ -0,0 +1,5 @@
+package services;
+
+public interface NotificacionesService {
+    void enviarNotificacion(String destino, String mensaje);
+}
